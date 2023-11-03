492758fa14d09195b6049e3ba8096644a963b868
Sergey <>
2023-11-03 16:55:49 +0400

    configure collection layout

---
+++ Today/Today/ReminderListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ReminderListViewController.swift
+//  Today
+//
+//  Created by Sergey on 03.11.2023.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+    }
+    
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
+
