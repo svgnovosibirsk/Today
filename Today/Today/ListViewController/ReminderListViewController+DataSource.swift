6557ed44165852984f51b2796094f294f0736b27
Sergey <>
2023-11-04 14:24:08 +0400

    add button to cell

--- Today/Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -19,8 +19,30 @@ extension ReminderListViewController {
         contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
         cell.contentConfiguration = contentConfiguration
         
+        var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
+        doneButtonConfiguration.tintColor = .todayListCellDoneButtonTint
+        
+        cell.accessories = [
+            .customView(configuration: doneButtonConfiguration),
+            .disclosureIndicator(displayed: .always)
+        ]
+        
         var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
         backgroundConfiguration.backgroundColor = .todayListCellBackground
         cell.backgroundConfiguration = backgroundConfiguration
+    }
+    
+    private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
+        let symbolName = reminder.isComplete ? "circle.fill" : "circle"
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        
+        let highlightedSymbolName = reminder.isComplete ? "star.fill" : "star"
+        let highlightedImage = UIImage(systemName: highlightedSymbolName, withConfiguration: symbolConfiguration)
+        button.setImage(highlightedImage, for: .highlighted)
+        
+        return UICellAccessory.CustomViewConfiguration(customView: button, placement: .leading(displayed: .always))
     }
 }
