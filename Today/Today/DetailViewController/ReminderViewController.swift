c08706844bd3fd119e3b8e5b40a6ee2c951d7e97
Sergey <>
2023-11-06 08:48:01 +0400

    add row enumeration

--- Today/Today/DetailViewController/ReminderViewController.swift
+++ Today/Today/DetailViewController/ReminderViewController.swift
@@ -21,4 +21,13 @@ class ReminderViewController: UICollectionViewController {
     required init?(coder: NSCoder) {
         fatalError("Always initialize ReminderViewController using init(reminder:)")
     }
+    
+    func text(for row: Row) -> String? {
+        switch row {
+        case .date: return reminder.dueDate.dayText
+        case .notes: return reminder.notes
+        case .time: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .title: return reminder.title
+        }
+    }
 }
