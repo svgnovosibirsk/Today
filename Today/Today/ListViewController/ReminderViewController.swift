fca473633bcde5a7fcdfa990acdce8fabbdcce8b
Sergey <>
2023-11-06 08:32:55 +0400

    add reminder view controller

---
+++ Today/Today/ListViewController/ReminderViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ReminderViewController.swift
+//  Today
+//
+//  Created by Sergey on 06.11.2023.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+    var reminder: Reminder
+    
+    init(reminder: Reminder) {
+        self.reminder = reminder
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfiguration.showsSeparators = false
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        super.init(collectionViewLayout: listLayout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+}
