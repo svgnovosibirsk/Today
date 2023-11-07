def69ffa07c07674111eb0fc79750c5eaee1309e
Sergey <>
2023-11-07 08:13:51 +0400

    add section type

--- Today/Today/Models/Date+Today.swift
+++ Today/Today/Models/Date+Today.swift
@@ -16,7 +16,7 @@ extension Date {
         } else {
             let dateText = formatted(.dateTime.month(.abbreviated).day())
             let dateAndTimeFormat = NSLocalizedString("%@ at %@", comment: "Date and time format string")
-            return String(format: dateAndTimeFormat, dateText)
+            return String(format: dateAndTimeFormat, dateText, timeText)
         }
     }
     
