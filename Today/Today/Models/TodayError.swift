fb7e8a3ecdac3be1bee59618ede372402e59377e
Sergey <>
2023-11-16 09:03:16 +0400

    implement ReminderStore

--- Today/Today/Models/TodayError.swift
+++ Today/Today/Models/TodayError.swift
@@ -8,18 +8,21 @@
 import Foundation
 
 enum TodayError: LocalizedError {
+    case accessDenied
     case failedReadingReminders
     case reminderHasNoDueDate
     
     var errorDescription: String? {
         switch self {
+        case .accessDenied:
+            return NSLocalizedString("The app doesn't have permission to read reminders.",
+                                     comment: "access denied error description")
         case .failedReadingReminders:
             return NSLocalizedString("Failed to read reminders.",
                                      comment: "failed reading reminders error description")
         case .reminderHasNoDueDate:
-                   return NSLocalizedString("A reminder has no due date.",
-                                            comment: "reminder has no due date error description")
+            return NSLocalizedString("A reminder has no due date.",
+                                     comment: "reminder has no due date error description")
         }
     }
-    
 }
