bdae95f226326e31b25fa6acc3e859a156465cb9
Sergey <>
2023-11-16 08:45:22 +0400

    implement ReminderStore and extension

---
+++ Today/Today/Models/ReminderStore.swift
@@ -0,0 +1,19 @@
+//
+//  ReminderStore.swift
+//  Today
+//
+//  Created by Sergey on 16.11.2023.
+//
+
+import EventKit
+import Foundation
+
+final class ReminderStore {
+    static let shared = ReminderStore()
+    
+    private let ekStore = EKEventStore()
+    
+    var isAvailable: Bool {
+        EKEventStore.authorizationStatus(for: .reminder) == .authorized
+    }
+}
