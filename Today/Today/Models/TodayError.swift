277bb12249e9ea6c7984f6d29917b1f21ca52218
Sergey <>
2023-11-17 09:00:29 +0400

    save reminders

--- Today/Today/Models/TodayError.swift
+++ Today/Today/Models/TodayError.swift
@@ -10,6 +10,7 @@ import Foundation
 enum TodayError: LocalizedError {
     case accessDenied
     case accessRestricted
+    case failedReadingCalendarItem
     case failedReadingReminders
     case reminderHasNoDueDate
     case unknown
@@ -22,6 +23,9 @@ enum TodayError: LocalizedError {
         case .accessRestricted:
             return NSLocalizedString("This device doesn't allow access to reminders.",
                                      comment: "access restricted error description")
+        case .failedReadingCalendarItem:
+                  return NSLocalizedString("Failed to read a calendar item.",
+                                           comment: "failed reading calendar item error description")
         case .failedReadingReminders:
             return NSLocalizedString("Failed to read reminders.",
                                      comment: "failed reading reminders error description")
